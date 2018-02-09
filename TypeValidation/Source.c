e56bc0991f36ce246d8ef6791798850590f5d9f6
emospy <emospy@gmail.com>
2018-02-09 10:18:01 +0200

    Some advice

--- TypeValidation/Source.c
+++ TypeValidation/Source.c
@@ -1,9 +1,14 @@
 #include<stdlib.h>
 #include <stdio.h>
 
-int key = 0;
+void Search(pNote head, int key);
+void addFirst(pNote *head); //Note **head 
+void printList(pNote head);
+void addLast(pNote *head);
+void FreeList(pNote *head);
+void Del(int key);
 
-typedef struct List 
+typedef struct List
 {
 	int key;
 	struct List *next;
@@ -13,43 +18,50 @@ typedef struct List
 typedef struct StuDate
 {
 	char name[20];
-	
 }*pDate;
-void Search(pNote head, int key);
-void addFirst( pNote *head); //Note **head 
-void printList(pNote head);
-void addLast(pNote *head);
-int main(void) 
+
+static int key = 0;
+
+int main(void)
 {
-	Note *head = NULL;
-	int n=0;
-	while (n != 6) {
-		addFirst(&head);
-		n++;
-	}
+	pNote head = NULL;
 	
+	int n = 0;
+	for (n = 0; n < 6; n++)
+	{
+		addFirst(&head);		
+	}
+
 	printList(head);
 	Search(head, 2);
-	
-	getchar();
+
+	getch();
 	return 0;
 }
+
 void addLast(pNote *head)
 {
 	pNote tmp = *head;
-	
+	pNote newS;
+
 	if (tmp == NULL)
 	{
-		tmp = malloc(sizeof(Note));
+		addFirst(head);
+	}
+	else
+	{
+		//find last
+		while (tmp->next != NULL)
+		{
+			tmp = tmp->next;
+		}
+
+		newS = malloc(sizeof(Note));
 		key++;
-		tmp->key = key;
-		tmp->next = *head;
-		*head = tmp;
-    }
-	tmp->next = malloc(sizeof(Note));
-	key++;
-	tmp->next->key = key;
-	tmp->next->next = NULL;
+		newS->key = key;
+		newS->next = NULL;
+		tmp->next = newS;
+	}
 }
 
 void addFirst(pNote *head)
@@ -57,10 +69,10 @@ void addFirst(pNote *head)
 	pNote tmp;
 	tmp = malloc(sizeof(Note));
 
-		key++;
-		tmp->key = key;
-		tmp->next = *head;
-		*head = tmp;
+	key++;
+	tmp->key = key;
+	tmp->next = *head;
+	*head = tmp;
 }
 
 
@@ -68,26 +80,38 @@ void printList(pNote head)
 {
 	pNote current;
 	current = head;
-	
-	while (current !=NULL)
-	{
-		printf("%d", current->key);
-		current = current -> next;
-	}
-
-}
-
-void Search(pNote head,int key)
-{
-	pNote current;
-	current = head;
 
 	while (current != NULL)
 	{
-		if (current->key == key) 
+		printf("%d", current->key);
+		current = current->next;
+	}
+}
+
+void Search(pNote head, int key)
+{
+	pNote current;
+	pNote prev;
+	current = head;
+
+	if (head->key == key)
+	{
+		printf(" \n Searched number: %d", current->key);
+	}
+
+	if (head->next != NULL)
+	{
+		current = head->next;
+		prev = head;
+	}
+
+	while (current != NULL)
+	{
+		if (current->key == key)
 		{
 			printf(" \n Searched number: %d", current->key);
 		}
 		current = current->next;
+		prev = prev->next;
 	}
 }
